3bd03d06507f3f930297b687fc7db88ef38f12bd
LightmanBro <omotosodavidromeo@gmail.com>
2024-07-07 18:05:00 +0100

    payaza api competition

--- payaza.js
+++ payaza.js
@@ -69,7 +69,7 @@ let data2 = JSON.stringify({
 let config2 = {
   method: 'post',
   maxBodyLength: Infinity,
-  url: 'https://router-live.78financials.com/api/request/secure/payloadhandler',
+  url: ' https://router-live.78financials.com/api/request/secure/payloadhandler',
   headers: { 
     'Authorization': 'Payaza UFo3OC1QS0xJVkUtMEUzRUVCRjYtQjU4Qi00REQyLTlDNzgtOTlEMUU2MTk0NUY4', 
     'X-TenantID': 'live', 
@@ -78,7 +78,49 @@ let config2 = {
   data : data2
 };
 
-// axios.request(config2)
+axios.request(config2)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
+// Response:{"response_code":201,"response_message":"Something went wrong doing request message integrity check.
+// Please contact customersupport@78financials.com"}
+
+
+let data = JSON.stringify({
+  "service_type": "Account",
+  "service_payload": {
+      "request_application": "Payaza",
+      "application_module": "USER_MODULE",
+      "application_version": "1.0.0",
+      "request_class": "GetAccountDetailsStaticAndDynamic",
+      "virtual_account_number": "4030058433"
+  }
+});
+
+// let config = {
+//   method: 'post',
+//   maxBodyLength: Infinity,
+//   url: ' https://router-live.78financials.com/api/request/secure/payloadhandler',
+//   headers: { 
+//     'X-TenantID': 'live', 
+//     'Authorization': 'Payaza UFo3OC1QS0xJVkUtMEUzRUVCRjYtQjU4Qi00REQyLTlDNzgtOTlEMUU2MTk0NUY4', 
+//     'Content-Type': 'application/json'
+//   },
+//   data : data
+// };
+
+/*
+{"response_code":200,"response_message":"Account Details Successful",
+"response_content":{"virtual_account_number":"4030058433",
+"virtual_account_name":"Payaza(David Omotoso)","account_type":"dynamic",
+"account_status":"Active","total_transactions":1,"provider_bank_name":"PREMIUM TRUST BANK",
+"provider_bank_code":"000031"},"status":"00"}
+*/
+// axios.request(config)
 // .then((response) => {
 //   console.log(JSON.stringify(response.data));
 // })
@@ -86,34 +128,43 @@ let config2 = {
 //   console.log(error);
 // });
 
-// Response:{"response_code":201,"response_message":"Something went wrong doing request message integrity check. Please contact customersupport@78financials.com"}
-
-
-let data = JSON.stringify({
+let reserve_account_detail = JSON.stringify({
+  "service_type": "Account",
   "service_payload": {
-    "currency": "NGN",
-    "bank_code": "000031",
-    "account_number": "4030058433"
+      "request_application": "Payaza",
+      "application_module": "USER_MODULE",
+      "application_version": "1.0.0",
+      "request_class": "CreateReservedAccountForCustomers",
+      "customer_first_name": "David",
+      "customer_last_name": "LightmanBro",
+      "customer_email": "lightmanbroinnovation@gmail.com",
+      "customer_phone": "08112878508",
+      "virtual_account_provider": "Providus"
   }
-});
+})
 
 let config = {
   method: 'post',
   maxBodyLength: Infinity,
-  url: 'https://api.payaza.africa/live/payaza-account/api/v1/mainaccounts/merchant/provider/enquiry',
+  url: ' https://router-live.78financials.com/api/request/secure/payloadhandler',
   headers: { 
     'X-TenantID': 'live', 
     'Authorization': 'Payaza UFo3OC1QS0xJVkUtMEUzRUVCRjYtQjU4Qi00REQyLTlDNzgtOTlEMUU2MTk0NUY4', 
     'Content-Type': 'application/json'
   },
-  data : data
+  data : reserve_account_detail
 };
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
-  
+/*
+{"response_code":200,"response_message":"Reserved Account Generated Successfully",
+"response_content":{"virtual_account_name":"PAYAZA(David LightmanBro)",
+"virtual_account_number":"9615411735","virtual_provider_bank_name":"PROVIDUS BANK",
+"virtual_provider_bank_code":"000023"}}
+*/
+// axios.request(config)
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
